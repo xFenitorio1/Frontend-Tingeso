ec93fc089757b79218c6dc142ec179f28b73c9bc
Gerardo Pérez <gerardo.perez@usach.cl>
2026-04-05 19:44:11 -0400

    change ports again :)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import keycloak from '../config/keycloak';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8090',
+  baseURL: 'http://localhost:8021',
   headers: {
     'Content-Type': 'application/json',
   },
