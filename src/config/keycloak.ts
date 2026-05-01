f9b3eb0ecc212f0e57960b8db5606531b51c75f2
Gerardo <gerardo.perez@usach.cl>
2026-05-01 00:28:07 -0400

    Patch please work v5

--- src/config/keycloak.ts
+++ src/config/keycloak.ts
@@ -2,6 +2,7 @@ import Keycloak from 'keycloak-js';
 
 const travelAgencyKeycloakServer = import.meta.env.VITE_TRAVELAGENCY_KEYCLOAK_SERVER
 
+
 const keycloakConfig = {
   url: `http://${travelAgencyKeycloakServer}`,
   realm: 'Tingeso',
@@ -9,5 +10,6 @@ const keycloakConfig = {
 };
 
 const keycloak = new Keycloak(keycloakConfig);
+(window as any).keycloak = keycloak; // <--- ESTO expone el objeto a la consola
 
 export default keycloak;
