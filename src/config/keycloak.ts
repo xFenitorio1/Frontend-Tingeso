9ddc28408f1551c5d0ee8ef5db181947f6ae96c3
Gerardo <gerardo.perez@usach.cl>
2026-04-30 22:38:15 -0400

    Using the environment variable for keycloak

--- src/config/keycloak.ts
+++ src/config/keycloak.ts
@@ -1,9 +1,21 @@
+// --- INICIO DEL PARCHE PARA HTTP ---
+if (typeof window !== 'undefined' && !window.crypto.randomUUID) {
+  Object.defineProperty(window.crypto, 'randomUUID', {
+    value: () => {
+      return (([1e7] as any) + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c: any) =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+      );
+    }
+  });
+}
+// --- FIN DEL PARCHE ---
+
 import Keycloak from 'keycloak-js';
 
 const travelAgencyKeycloakServer = import.meta.env.VITE_TRAVELAGENCY_KEYCLOAK_SERVER
 
 const keycloakConfig = {
-  url: 'http://167.172.246.45/auth',
+  url: `http://${travelAgencyKeycloakServer}`,
   realm: 'Tingeso',
   clientId: 'frontend-client',
 };
